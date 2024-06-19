e2aa25b29f473cb003231dc4cc0ed567f0f208b6
yass <yass@takibi.net>
2024-06-19 12:34:35 +0900

    Fix filename

--- GSS/docker/start.sh
+++ GSS/docker/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker compose -f docker_compose.yml build
-docker compose -f docker_compose.yml up -d
+docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml up -d
